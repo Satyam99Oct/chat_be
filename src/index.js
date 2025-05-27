c908bde7989f576f3110f5ce9a4c07591469a8fa
Satyam Yadav <satyam99ime@gmail.com>
2025-05-27 10:55:08 +0530

    fic

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
@@ -20,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => callback(null, origin),
     credentials: true,
   })
 );
@@ -30,7 +27,6 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
